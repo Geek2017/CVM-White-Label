3dbad43d39421a8c3693029eeaadedc0837d20b9
Geektech <jmlgrefaldo@bicol-u.edu.ph>
2021-04-13 06:18:46 +0800

    all

--- public/controllers/salesproposalCtrl.js
+++ public/controllers/salesproposalCtrl.js
@@ -260,34 +260,27 @@ angular.module('newApp').controller('salesproposalCtrl', function($scope) {
     var signaturePad = new SignaturePad(c);
     signaturePad.fromDataURL(data);
 
-
-
+    let ukeyid = localStorage.getItem('ukeyid');
 
     $scope.passwordcall = function(length, special) {
-        var iteration = 0;
-        var password = "";
-        var randomNumber;
-        if (special == undefined) {
-            var special = false;
-        }
-        while (iteration < length) {
-            randomNumber = (Math.floor((Math.random() * 100)) % 94) + 33;
-            if (!special) {
-                if ((randomNumber >= 33) && (randomNumber <= 47)) { continue; }
-                if ((randomNumber >= 58) && (randomNumber <= 64)) { continue; }
-                if ((randomNumber >= 91) && (randomNumber <= 96)) { continue; }
-                if ((randomNumber >= 123) && (randomNumber <= 126)) { continue; }
-            }
-            iteration++;
-            password += String.fromCharCode(randomNumber);
-        }
-        // document.getElementById('hash').innerHTML=password;
 
-        $scope.hash = window.location.origin + '/formsindex.html#/:' + password;
+
+
+        $scope.hash = window.location.origin + '/formsindex.html#/:' + ukeyid;
         console.log(password);
         sessionStorage.setItem('hkc', password);
     }
 
+
+    $scope.closesp = function() {
+        $('#spsuccess').removeClass('open')
+        window.location.replace("#/");
+        window.location.replace("#/salesproposal");
+    }
+
+    $scope.goindex = function() {
+        window.location.replace("#/spindex");
+    }
 
     $("#myform").submit(function(event) {
 
@@ -346,6 +339,8 @@ angular.module('newApp').controller('salesproposalCtrl', function($scope) {
         try {
             var uid = firebase.database().ref().child('salesproposal').push().key;
 
+            localStorage.setItem('ukeyid', uid);
+
             var data = {
                 mrc: table0,
                 nrc: table1,
@@ -380,12 +375,13 @@ angular.module('newApp').controller('salesproposalCtrl', function($scope) {
 
             if (updates) {
                 $('#spsuccess').addClass('open')
-                setTimeout(function() {
-                    window.location.replace("#/");
-                    window.location.replace("#/spindex");
-                }, 2000)
+                    // setTimeout(function() {
+                    //     window.location.replace("#/");
+                    //     window.location.replace("#/spindex");
+                    // }, 2000)
 
             }
+
 
         } catch (err) {
             $("#errormsg").text(err);
--- public/controllers/spindexCrtl.js
+++ public/controllers/spindexCrtl.js
@@ -293,23 +293,7 @@ angular.module('newApp').controller('spindexCrtl', function($scope, $timeout) {
 
 
     $scope.passwordcall = function(length, special) {
-        var iteration = 0;
-        var password = "";
-        var randomNumber;
-        if (special == undefined) {
-            var special = false;
-        }
-        while (iteration < length) {
-            randomNumber = (Math.floor((Math.random() * 100)) % 94) + 33;
-            if (!special) {
-                if ((randomNumber >= 33) && (randomNumber <= 47)) { continue; }
-                if ((randomNumber >= 58) && (randomNumber <= 64)) { continue; }
-                if ((randomNumber >= 91) && (randomNumber <= 96)) { continue; }
-                if ((randomNumber >= 123) && (randomNumber <= 126)) { continue; }
-            }
-            iteration++;
-            password += String.fromCharCode(randomNumber);
-        }
+
         // document.getElementById('hash').innerHTML=password;
 
         $scope.hash = window.location.origin + '/formsindex.html#/:' + password;
@@ -473,7 +457,6 @@ angular.module('newApp').controller('spindexCrtl', function($scope, $timeout) {
             firebase.database().ref().update(updates);
             console.log(updates)
 
-            $scope.successtxt = updates;
 
             if (updates) {
                 $('#spsuccess').addClass('open')
